9c2decc1b487c968d7a8468747471906f1a5a3cc
Norhan Swar <eng-nourhan.sowar1722@alexu.edu.eg>
2023-12-09 23:17:32 +0200

    Docker Hadoop Spark Cluster for Stroing and Getting real insights

---
+++ E-commerce Customer Behavior/start-spark.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+. "/opt/spark/bin/load-spark-env.sh"
+hadoop fs -mkdir       /tmp
+hadoop fs -mkdir -p    /user/hive/warehouse
+hadoop fs -chmod g+w   /tmp
+hadoop fs -chmod g+w   /user/hive/warehouse
+
+cd $HIVE_HOME/bin
+./hiveserver2 --hiveconf hive.server2.enable.doAs=false
+
+if [ "$SPARK_WORKLOAD" == "master" ];
+then
+
+export SPARK_MASTER_HOST=`hostname`
+
+cd /opt/spark/bin && ./spark-class org.apache.spark.deploy.master.Master --ip $SPARK_MASTER_HOST --port $SPARK_MASTER_PORT --webui-port $SPARK_MASTER_WEBUI_PORT >> $SPARK_MASTER_LOG
+
+elif [ "$SPARK_WORKLOAD" == "worker" ];
+then
+
+cd /opt/spark/bin && ./spark-class org.apache.spark.deploy.worker.Worker --webui-port $SPARK_WORKER_WEBUI_PORT $SPARK_MASTER >> $SPARK_WORKER_LOG
+
+elif [ "$SPARK_WORKLOAD" == "submit" ];
+then
+    echo "SPARK SUBMIT"
+else
+    echo "Undefined Workload Type $SPARK_WORKLOAD, must specify: master, worker, submit"
+fi
+
+
